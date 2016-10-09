e8706b03a807e0ae41a90e74f04a9d7c13613ebc
Achintha.Reemal <Achintha.Reemal@trycake.com>
2016-10-09 19:05:29 +0530

    Adding comments

--- Step_09/server.js
+++ Step_09/server.js
@@ -12,24 +12,32 @@ var database;
 // url of the mongodb database which we are using (username:cake_user , pw:1qaz2wsx)
 var dbUrl = 'mongodb://cake_user:1qaz2wsx@ds049466.mlab.com:49466/cake_it_workshop';
 
-
+//Connecting to the database
 MongoClient.connect(dbUrl, function (err, db) {
     if (err) {
         return console.log(err);
     }
     database = db;
+    //start server only when the database connection is a success
     app.listen(3000, function () {
         console.log('Demo Listening on 3030')
     })
 });
 
+//setting the view engine to EJS (Embedded JavaScript)
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+//route method to handle GET requests, which will serve an HTML
+//file when the users access '/' route
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+
+// route method to handle POST requests which are generated when users
+// submit form data. This will save the form data in a document created
+// inside the 'students' collection.
 app.post('/register', function (req, res) {
     database.collection('students').save(req.body, function (err, result) {
         if (err) {
@@ -40,11 +48,16 @@ app.post('/register', function (req, res) {
     })
 });
 
+// route method to handle GET requests which are generated when users
+// access '/students' route. This will retrieve all the documents from
+// 'students' collection and display in a table.
 app.get('/students', function (req, res) {
     database.collection('students').find().toArray(function (err, results) {
         if (err) {
             return console.log(err);
         }
+
+        //render the view file dynamically with the results array
         res.render('students.ejs', {students: results});
     })
 });
